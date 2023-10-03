7bc5f439db76864f50230fbc1b3b6975d552cad0
AlexPozdniak <sotich911@gmail.com>
2023-10-03 12:36:19 +0300

    поменять ip

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -8,8 +8,8 @@ const allowedCors = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:4000',
-  'https://158.160.104.64',
-  'http://158.160.104.64',
+  'https://158.160.121.97',
+  'http://158.160.121.97',
 ];
 
 module.exports = (req, res, next) => {
